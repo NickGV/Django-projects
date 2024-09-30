71ab757ac919fd0b080fc25f180fee21f921f0c5
Nick <nickgomvelez@gmail.com>
2024-09-30 10:48:16 -0500

    finish blog MVP

--- Blog_app/blog/models.py
+++ Blog_app/blog/models.py
@@ -7,14 +7,14 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
 
--- Blog_app/blog/views.py
+++ Blog_app/blog/views.py
@@ -1,28 +1,44 @@
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(title__icontains=query)
+        return Post.objects.all()
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView( CreateView):
     model = Post
     template_name = 'blog/post_form.html'
     fields = ['title', 'content']
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+class PostUpdateView(UpdateView):
     model = Post
     template_name = 'blog/post_form.html'
     fields = ['title', 'content']
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+class PostDeleteView(DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('home')
+
+    def get_success_url(self):
+        return reverse_lazy('home')
